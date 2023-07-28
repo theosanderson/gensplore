5ab198fb744f2a771647575d62960bcd532533b9
Theo Sanderson <theo@theo.io>
2023-07-28 21:53:50 +0100

    update

--- src/App.js
+++ src/App.js
@@ -510,6 +510,15 @@ function GensploreView({ genbankString, searchInput, setSearchInput }) {
         </label>
       </p>
 
+      <div className="mt-4 flex justify-end">
+        <button
+          type="button"
+          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+          onClick={() => setConfigModalOpen(false)}
+        >
+          Close
+        </button>
+        </div>
     </div>
   </Dialog.Panel>
 </Dialog>
