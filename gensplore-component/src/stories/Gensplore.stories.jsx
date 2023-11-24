cb10506955123a2de9a2fd034d35465d99ffd02a
Theo Sanderson <theo@theo.io>
2023-11-24 16:33:22 +0000

    update

--- gensplore-component/src/stories/Gensplore.stories.jsx
+++ gensplore-component/src/stories/Gensplore.stories.jsx
@@ -9,7 +9,7 @@ export default {
 
 const Template = (args) => <GensploreComponent genbankString={
 `LOCUS       Sample100bp       100 bp    DNA     linear   UNK 24-NOV-2023
-DEFINITION  Synthetic construct, 100 bp DNA fragment.
+DEFINITION  Synthetic construct, 100 bp DNA fragment with 42 bp coding sequence.
 ACCESSION   XXX00000000
 VERSION     XXX00000000.1
 KEYWORDS    .
@@ -21,14 +21,19 @@ REFERENCE   1  (bases 1 to 100)
   TITLE     Direct Submission
   JOURNAL   Submitted (24-NOV-2023) to the GenBank
 FEATURES             Location/Qualifiers
-     CDS             1..100
+     source          1..100
                      /organism="synthetic construct"
                      /mol_type="genomic DNA"
                      /db_xref="taxon:000000"
+     CDS             30..71
+                     /codon_start=1
+                     /product="mini protein"
+                     /protein_id="AAA00000.1"
 ORIGIN      
-        1 atgctagctg acgtacgtac gtagctagct agctagctac gtagctagct agctagctag
-       61 ctgactgact gactgactga ctgactgact gactgactga
-//`
+        1 atgctagcta cgtagctatg ctagctagct agctagctag ctgatgaaaa aaaaaa
+       61 atatattata actgactgac tgactgactg actgactgac
+//
+`
 }/>;
 
 export const Primary = Template.bind({});
